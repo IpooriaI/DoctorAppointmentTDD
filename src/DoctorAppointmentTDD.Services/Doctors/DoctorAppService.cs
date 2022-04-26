18e20f3fa3dc44a421369a0e3992f0fbcb32bd4e
Pooria <pooriayoo09@gmail.com>
2022-04-26 12:44:54 +0430

    added appservices

---
+++ src/DoctorAppointmentTDD.Services/Doctors/DoctorAppService.cs
@@ -0,0 +1,14 @@
+﻿using DoctorAppointmentTDD.Services.Doctors.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorAppointmentTDD.Services.Doctors
+{
+    public class DoctorAppService : DoctorService
+    {
+        
+    }
+}
---
+++ src/DoctorAppointmentTDD.Services/Patients/PatientAppService.cs
@@ -0,0 +1,14 @@
+﻿using DoctorAppointmentTDD.Services.Patients.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorAppointmentTDD.Services.Patients
+{
+    public class PatientAppService : PatientService
+    {
+
+    }
+}
