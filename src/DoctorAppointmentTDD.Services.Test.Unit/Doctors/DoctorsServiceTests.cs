3d4e7a67c833d1777cce6a224f053bb77e0e5450
Pooria <pooriayoo09@gmail.com>
2022-04-26 13:02:19 +0430

    added in memory database for tests

---
+++ src/DoctorAppointmentTDD.Services.Test.Unit/Doctors/DoctorsServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using DoctorAppointmentTDD.Infrastructure.Application;
+using DoctorAppointmentTDD.Services.Doctors.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorAppointmentTDD.Services.Test.Unit.Doctors
+{
+    public class DoctorsServiceTests
+    {
+        private readonly EFDataContext _dataContext;
+        private readonly UnitOfWork _unitOfWork;
+        private readonly DoctorService _sut;
+        private readonly DoctorRepository _repository;
+    }
+}
--- src/DoctorAppointmentTDD.Services/Appointments/AppointmentAppService.cs
+++ src/DoctorAppointmentTDD.Services/Appointments/AppointmentAppService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoctorAppointmentTDD.Services.Appointments.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace DoctorAppointmentTDD.Services.Appointments
 {
-    internal class AppointmentAppService
+    public class AppointmentAppService : AppointmentService
     {
+
+
     }
 }
