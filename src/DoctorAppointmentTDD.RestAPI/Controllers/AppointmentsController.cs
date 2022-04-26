ece8e1a9dfc26cbb724996500db59171dc3a2685
Pooria <pooriayoo09@gmail.com>
2022-04-26 12:48:10 +0430

    added empty controllers

---
+++ src/DoctorAppointmentTDD.RestAPI/Controllers/AppointmentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorAppointmentTDD.RestAPI.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/DoctorAppointmentTDD.RestAPI/Controllers/DoctorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorAppointmentTDD.RestAPI.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
